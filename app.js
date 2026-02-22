d8e41d0ae282ab27d038cca6d8d4d0b9ca58fad5
Gayal Thilakarathna <thilakarathnagayal@gmail.com>
2026-02-22 11:25:29 +0530

    Added async/await feature

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const http = require('http');
- http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-   res.end();
- }).listen(3000);  // Using port 3000 to avoid conflicts
- console.log('Server running on port 3000');
+
+ const condition = true;
+ const myPromise = new Promise((resolve, reject) => {
+   if(condition){ resolve('Success!'); } else { reject('Failure!'); }
+ });
+
+ async function myFunction() {
+   try {
+     const result = await myPromise;
+     console.log(result);
+   } catch (error) {
+    console.log(error);
+  }
+}
+ myFunction();
+
